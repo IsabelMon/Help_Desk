db8596062d8ecb5f75acc1529deb12ea679bd1d6
Ana <montesarroyavea@gmail.com>
2023-08-16 14:54:18 -0500

    ya gurda datos solo que en solicitado por: necesesita que se auto complete el cargo a su vez que se selecciona el cargo

--- src/controllers/google.controlle.js
+++ src/controllers/google.controlle.js
@@ -1,20 +1,9 @@
-//const obtenerInformes = '../controllers/google.controller'; // Asegúrate de que esta ruta sea correcta
 let GoogleSheet = require('../spreadsheet');
-
-
-const obtenerInformes = async (req,res)=>{
-    readDataResponse = await GoogleSheet.accederGoogleSheet();
-    res.render('index',{readDataResponse})
-}
-
 const escribirEnGoogleSheet = async (req,res)=>{
-
   GoogleSheet.escribirEnGoogleSheet(req.body);
-
 }
-
 module.exports = {
-    obtenerInformes,  
     escribirEnGoogleSheet,
+    
 };
   
