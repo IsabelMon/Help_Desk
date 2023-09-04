1d489d24dad4c358b0620a899ae97731f6ed5f8a
Ana <montesarroyavea@gmail.com>
2023-09-03 22:06:32 -0500

    Se agrego el login

--- src/app.js
+++ src/app.js
@@ -1,38 +1,63 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { escribirEnGoogleSheet } = require('./spreadsheet'); // Importa la función escribirEnGoogleSheet
-const usuariosCargos = require('./views/usuariosCargos');
+const usuariosCargos = require('./views/js/usuariosCargos');
 const app = express();
 const path = require('path')
+const cors = require('cors')
 
-//app.set('views', './src/views');
-///app.set('view engine', 'pug');
-// si no lleva esto no funciona , es lo que maneja el body
+
+app.use(cors())
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static('views'));
+app.use(function (req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+	res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+	next();
+
+})
+
+app.use('/static', express.static(path.join(__dirname, 'views')))
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+  res.sendFile(path.join(__dirname, 'views/html/login.html'));
 });
-app.get('/buscarequipos.html', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', '/buscarequipos.html'));
+
+app.get('/hojaEncuesta', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views/html/hojaEncuesta.html'));
+});
+app.get('/buscarequipos', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views/html/buscarequipos.html'));
+});
+
+app.get('/sistemas', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views/html/sistemas.html'));
+});
+
+app.get('/dosificacion', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views/html/dosificacion.html'));
 });
 
 
 app.get('/usuariosCargos.js', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'usuariosCargos.js'));
+  res.sendFile(path.join(__dirname, 'views/js/usuariosCargos.js'));
 });
 
+app.get('/login.js', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views/js/login.js'));
+});
 app.get('/script.js', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'script.js'));
+  res.sendFile(path.join(__dirname, 'views/js/script.js'));
 });
 app.get('/typeahead.js', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'typeahead.js'));
+  res.sendFile(path.join(__dirname, 'views/js/typeahead.js'));
 });
 
 app.get('/', (req, res) => {
-  res.render('index', { usuariosCargos }); // Pasa el objeto usuariosCargos a la vista
+  res.render('hojaEncuesta', { usuariosCargos }); // Pasa el objeto usuariosCargos a la vista
 });
 
 
---
+++ src/views/js/login.js
@@ -0,0 +1,11 @@
+function iniciarSesion() {
+    console.log("esta ")
+    const tipoUsuario = document.getElementById('tipoUsuario').value;
+    
+    // Aquí puedes agregar la lógica para redirigir al usuario según su selección
+    if (tipoUsuario === 'Sistemas') {
+        window.location.href = 'sistemas'; // Redirige a la página de Sistemas
+    } else if (tipoUsuario === 'Dosificación') {
+        window.location.href = 'dosificacion'; // Redirige a la página de Dosificación
+    }
+}
