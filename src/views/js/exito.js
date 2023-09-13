3c59c22df7f207f5fa6bd7070fc40c76f182d195
Ana <montesarroyavea@gmail.com>
2023-09-13 11:22:24 -0500

    Se coloca  la  alerta

--- src/views/js/exito.js
+++ src/views/js/exito.js
@@ -1,3 +1,13 @@
-var loaderOverlay = document.getElementById('pantalla-exito');
+function pantallaexito(){
+ var loaderOverlay = document.getElementById('pantalla-exito');
 loaderOverlay.style.display = 'flex';
+
+setTimeout(function(){
+    location.reload();
+},1000)
+document.addEventListener(clic)
+
+
+} 
+
 //reiniciamos la pagina
